1457b5f532d39a3b396f753084ce5d6ce1b1b863
Sean Pollock <seanpollock3344@gmail.com>
2017-10-11 14:06:35 -0700

    test rinkeby tx and rpc call to get tx info

--- index.js
+++ index.js
@@ -4,12 +4,27 @@ import RPC from 'ethjs-rpc'
 export class MerkleProof {
   constructor(params) {
     this.eth = new RPC(new Provider(params.provider))
-    console.log('this.eth', this.eth)
+  }
+
+  async getTransactionProof(txHash) {
+    try {
+      console.log('txHash', txHash)
+      const tx = await this.eth.sendAsync({
+        method: 'eth_getTransactionByHash',
+        params: [txHash]
+      })
+      console.log('tx', tx)
+    } catch (err) {
+      Error('### error in getTransactionProof', err)
+    }
   }
 }
 
+const txHash = '0x9cbf883dbf97bd0c4c7993e10b22f5325a91fc032b38682929ce112ad644632d'
 const params = {
-  provider : 'https://gmainnet.infura.io'
+  provider : 'https://rinkeby.infura.io'
 }
 
-new MerkleProof(params)
+const MP = new MerkleProof(params)
+
+MP.getTransactionProof(txHash).then(prf => console.log('proof', prf))
